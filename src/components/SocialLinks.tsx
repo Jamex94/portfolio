e99f40134cc31d8757687fd5ae16672fbcf08275
Jamex94 <113483014+Jamex94@users.noreply.github.com>
2023-08-29 21:25:16 -0500

    portfolio

---
+++ src/components/SocialLinks.tsx
@@ -0,0 +1,71 @@
+import { motion } from 'framer-motion'
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'
+import { IoIosMail } from 'react-icons/io'
+
+interface SocialLinkProps {
+  className?: string
+}
+
+export function ResumeLink ({ className = '' }: SocialLinkProps): JSX.Element {
+  return (
+    <motion.a
+      href='https://drive.google.com/file/d/1jp6rMu7qkirw_nSrkqMWhBHx9WC-BvmH/view?usp=drive_link'
+      target='_blank'
+      rel='noopener noreferrer'
+      whileHover={{ scale: 1.05 }}
+      whileTap={{ scale: 0.95 }}
+      title={'View James\'s resume'}
+      className={className}
+    >
+      Resume
+    </motion.a>
+  )
+}
+
+export function GithubLink ({ className = '' }: SocialLinkProps): JSX.Element {
+  return (
+    <motion.a
+      href='https://github.com/jamex94'
+      target='_blank'
+      rel='noopener noreferrer'
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+      title={'Go to James\'s GitHub'}
+      className={className}
+    >
+      <FaGithub />
+    </motion.a>
+  )
+}
+
+export function LinkedinLink ({ className = '' }: SocialLinkProps): JSX.Element {
+  return (
+    <motion.a
+      href='https://linkedin.com/in/jamextrudeau'
+      target='_blank'
+      rel='noopener noreferrer'
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+      title='Connect with James on LinkedIn'
+      className={className}
+    >
+      <FaLinkedin />
+    </motion.a>
+  )
+}
+
+export function MailLink ({ className = '' }: SocialLinkProps): JSX.Element {
+  return (
+    <motion.a
+      href='mailto:alex.trudeau@engineer.com'
+      target='_blank'
+      rel='noopener noreferrer'
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+      title='Send James an email'
+      className={className}
+    >
+      <IoIosMail style={{ transform: 'scale(1.2)' }} />
+    </motion.a>
+  )
+}
---
+++ src/data/projects.ts
@@ -0,0 +1,45 @@
+export interface ProjectType {
+  name: string
+  description: string
+  image: string
+  codeUrl?: string
+  demoUrl?: string
+  tools: string[]
+}
+
+export const projectsList: ProjectType[] = [
+  {
+    name: 'Pato Click',
+    description:
+      'Duck-click is an idle game!',
+    image: 'wordle.webp',
+    codeUrl: 'https://github.com/Jamex94/pato-click',
+    demoUrl: 'https://pato-click-jamex.vercel.app',
+    tools: ['JSX', 'React', 'CSS', 'Vercel']
+  },
+  {
+    name: 'Dev Blog',
+    description: 'My personal blog where I share some of my coding knowledge and experience.',
+    image: 'devBlog.webp',
+    codeUrl: 'https://github.com/Jamex94/Dev-Blog',
+    demoUrl: 'https://dev-blog-jamex.vercel.app',
+    tools: ['Typescript', 'NextJS', 'Tailwind CSS', 'MDX']
+  },
+  {
+    name: 'Art Gallery',
+    description:
+      'Art portfolio website that I built for the pintor Alberto Revelo.\nA place to showcase her art and connect with her audience.',
+    image: 'artPortfolio.webp',
+    codeUrl: 'https://github.com/Jamex94/art-gallery',
+    demoUrl: 'https://art-gallery-jamex.vercel.app',
+    tools: ['Typescript', 'NextJS', 'Tailwind CSS']
+  },
+  {
+    name: 'Code Toolkit @Jamex',
+    description:
+      'My own code utilities and two React libraries. Published on NPM and available for use.\nEmpowering developers to build faster and better!',
+    image: 'npmLibraries.webp',
+    codeUrl: 'https://github.com/Jamex94/Jamex',
+    tools: ['Typescript', 'React', 'NPM', 'Turborepo']
+  }
+]
